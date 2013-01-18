a1cad9078367020fbff6f1ec7f215f4d90c7d78e
Alexander Hripak <alex@left-click.us>
2013-01-18 12:38:57 -0500

    Workaround for services boolean field bug

--- demo/sites/all/modules/custom/todo4u/todo4u.js
+++ demo/sites/all/modules/custom/todo4u/todo4u.js
@@ -1,9 +1,41 @@
 (function($) {
+  /**
+   * Super hacky solution to change a textfield into a checkbox.
+   * The purpose of this is to workaround a services bug that
+   * doesn't handle boolean fields properly.
+   */
   Drupal.behaviors.todo4u = {
     attach: function(context) {
-      // Use clicking on the checkbox as if it were the save button to trigger ajax
-      $('.form-item-field-complete-und .form-checkbox').click(function() {
-	$(this).parent().parent().parent().parent().parent().find('.form-submit').click();
+      // Target the textfields
+      $('input[type="text"][name^="field_complete"]').each(function() {
+	// Make a clone and hide the textfield
+	$copy = $(this).clone();
+	$(this).hide();
+
+	// Modify some props and add a click listener
+	$copy.addClass('faux_check').attr({
+	  type : 'checkbox'
+	}).click(function() {
+	  // Need to fill in the proper value in the textfield
+	  $(this).parent().find('input[type="text"]').val($(this).attr('checked') == true ? '1' : '0');
+	  // Get the form selector before we remove the checkbox
+	  var $form = $(this).parent().parent().parent().parent().parent().find('.form-submit');
+	  // For validation purposes, remove the checkbox
+	  $(this).remove();
+	  $form.click();
+	});
+
+	// This is called on ajaxSuccess or dom.ready
+	if($(this).val() == "1") {
+	  $copy.attr({
+	    checked : 'checked'
+	  });
+	}
+
+	// Only append the checkbox if one doesn't already exist
+	if($(this).parent().children().length == 1) {
+	  $(this).parent().append($copy);
+	}
       });
     }
   }
